dc2ff63e243d88d08c753b343e3486846a6c0e6d
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2023-03-16 22:47:23 +0700

    test

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ connectMG();
 routes(app);
 let dataZalo = {};
 app.get("/", (req, res) => {
-  res.json({ test: "Hello World 2!" });
+  res.json({ test: "Hello World 13!" });
 });
 
 app.post("/zalopay", (request, response) => {
